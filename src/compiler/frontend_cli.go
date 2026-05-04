3a3ec3eea31b9631982b45bfed4db6c0ac5c14c9
Jevin Sweval <jevinsweval@gmail.com>
2026-05-04 13:48:14 -0400

    compiler cli arg parsing WIP

--- src/compiler/frontend_cli.go
+++ src/compiler/frontend_cli.go
@@ -5,47 +5,82 @@ package main
 import (
 	"fmt"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/thediveo/enumflag/v2"
 )
 
+type BundleFormatKind enumflag.Flag
+
+const (
+	Esm BundleFormatKind = iota
+	Iife
+)
+
+var BundleFormatKindIds = map[BundleFormatKind][]string{
+	Esm:  {"esm"},
+	Iife: {"iife"},
+}
+var bundleformat BundleFormatKind = Esm
+
+type TypeCheckKind enumflag.Flag
+
+const (
+	Full TypeCheckKind = iota
+	None
+)
+
+var TypeCheckKindIds = map[TypeCheckKind][]string{
+	Full: {"full"},
+	None: {"none"},
+}
+var typecheck TypeCheckKind = Full
+
+type PlatformKind enumflag.Flag
+
+const (
+	Gum PlatformKind = iota
+	Browser
+	Neutral
+)
+
+var PlatformKindIds = map[PlatformKind][]string{
+	Gum:     {"gum"},
+	Browser: {"browser"},
+	Neutral: {"neutral"},
+}
+var platform PlatformKind = Gum
+
+var external = []string{}
+
 func main() {
-	// ①+② skip "define your own enum flag type" and enumeration values, as we
-	// already have a 3rd party one.
-
-	// ③ Map 3rd party enumeration values to their textual representations
-	var LoglevelIds = map[log.Level][]string{
-		log.TraceLevel: {"trace"},
-		log.DebugLevel: {"debug"},
-		log.InfoLevel:  {"info"},
-		log.WarnLevel:  {"warning", "warn"},
-		log.ErrorLevel: {"error"},
-		log.FatalLevel: {"fatal"},
-		log.PanicLevel: {"panic"},
-	}
-
-	// ④ Define your enum flag value and set the your logging default value.
-	var loglevel log.Level = log.WarnLevel
-
 	rootCmd := &cobra.Command{
 		Run: func(cmd *cobra.Command, _ []string) {
-			fmt.Printf("logging level is: %d=%q\n",
-				loglevel,
-				cmd.PersistentFlags().Lookup("log").Value.String())
+			fmt.Printf("bundle-format is: %d=%q\n",
+				bundleformat,
+				cmd.PersistentFlags().Lookup("bundle-format").Value.String())
+			fmt.Printf("type-check is: %d=%q\n",
+				typecheck,
+				cmd.PersistentFlags().Lookup("type-check").Value.String())
+			fmt.Printf("platform is: %d=%q\n",
+				platform,
+				cmd.PersistentFlags().Lookup("platform").Value.String())
+			fmt.Printf("external is: %d\n",
+				external)
 		},
 	}
+	rootCmd.PersistentFlags().VarP(
+		enumflag.New(&bundleformat, "bundle-format", BundleFormatKindIds, enumflag.EnumCaseSensitive),
+		"bundle-format", "B",
+		"desired bundle format")
+	rootCmd.PersistentFlags().VarP(
+		enumflag.New(&typecheck, "type-check", TypeCheckKindIds, enumflag.EnumCaseSensitive),
+		"type-check", "T",
+		"desired type-checking mode")
+	rootCmd.PersistentFlags().VarP(
+		enumflag.New(&platform, "platform", PlatformKindIds, enumflag.EnumCaseSensitive),
+		"platform", "",
+		"JavaScript runtime platform")
+	rootCmd.Flags().StringArrayVarP(&external, "external", "E", []string{}, "mark MODULE as external (may be specified multiple times)")
 
-	// ⑤ Define the CLI flag parameters for your wrapped enum flag.
-	rootCmd.PersistentFlags().Var(
-		enumflag.New(&loglevel, "log", LoglevelIds, enumflag.EnumCaseInsensitive),
-		"log",
-		"sets logging level; can be 'trace', 'debug', 'info', 'warn', 'error', 'fatal', 'panic'")
-
-	// Defaults to what we set above: warn level.
-	_ = rootCmd.Execute()
-
-	// User specifies a specific level, such as log level.
-	rootCmd.SetArgs([]string{"--log", "debug"})
 	_ = rootCmd.Execute()
 }
--- src/compiler/go.mod
+++ src/compiler/go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/go-json-experiment/json v0.0.0-20260214004413-d219187c3433 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
-	github.com/spf13/pflag v1.0.9 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/zeebo/xxh3 v1.1.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
