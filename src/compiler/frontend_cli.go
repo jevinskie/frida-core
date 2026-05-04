2f4955b625e416349c2d6f7c26fd698cf0602f3f
Jevin Sweval <jevinsweval@gmail.com>
2026-05-04 14:17:52 -0400

    frontend_cli.go wip

--- src/compiler/frontend_cli.go
+++ src/compiler/frontend_cli.go
@@ -4,10 +4,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/thediveo/enumflag/v2"
 )
+
+var module string
+
+var output string
+
+var compress bool
+
+var verbose bool
 
 type BundleFormatKind enumflag.Flag
 
@@ -52,22 +61,16 @@ var platform PlatformKind = Gum
 
 var external = []string{}
 
-func main() {
-	rootCmd := &cobra.Command{
-		Run: func(cmd *cobra.Command, _ []string) {
-			fmt.Printf("bundle-format is: %d=%q\n",
-				bundleformat,
-				cmd.PersistentFlags().Lookup("bundle-format").Value.String())
-			fmt.Printf("type-check is: %d=%q\n",
-				typecheck,
-				cmd.PersistentFlags().Lookup("type-check").Value.String())
-			fmt.Printf("platform is: %d=%q\n",
-				platform,
-				cmd.PersistentFlags().Lookup("platform").Value.String())
-			fmt.Printf("external is: %d\n",
-				external)
-		},
-	}
+var rootCmd = &cobra.Command{
+	Use:  "frida-core-compiler MODULE",
+	Run:  run,
+	Args: cobra.ExactArgs(1),
+}
+
+func init() {
+	// rootCmd.PersistentFlags().String(&module, "TypeScript/JavaScript module to compile")
+	rootCmd.PersistentFlags().BoolVarP(&compress, "compress", "c", false, "minify code")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "be verbose")
 	rootCmd.PersistentFlags().VarP(
 		enumflag.New(&bundleformat, "bundle-format", BundleFormatKindIds, enumflag.EnumCaseSensitive),
 		"bundle-format", "B",
@@ -81,6 +84,34 @@ func main() {
 		"platform", "",
 		"JavaScript runtime platform")
 	rootCmd.Flags().StringArrayVarP(&external, "external", "E", []string{}, "mark MODULE as external (may be specified multiple times)")
+}
 
-	_ = rootCmd.Execute()
+func run(cmd *cobra.Command, args []string) {
+	module = args[0]
+	fmt.Printf("cmd: %v\n", cmd)
+	fmt.Printf("args: %v\n", args)
+	fmt.Printf("module is: %v\n",
+		module)
+	fmt.Printf("compress is: %v\n",
+		compress)
+	fmt.Printf("verbose is: %v\n",
+		verbose)
+	fmt.Printf("bundle-format is: %v=%q\n",
+		bundleformat,
+		cmd.PersistentFlags().Lookup("bundle-format").Value.String())
+	fmt.Printf("type-check is: %v=%q\n",
+		typecheck,
+		cmd.PersistentFlags().Lookup("type-check").Value.String())
+	fmt.Printf("platform is: %v=%q\n",
+		platform,
+		cmd.PersistentFlags().Lookup("platform").Value.String())
+	fmt.Printf("external is: %v\n",
+		external)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
